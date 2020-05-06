86c331890bb6824f1802e9b630714f99b772188d
bigyoung <bigyoung@bigyoungdeMacBook-Pro.local>
2020-05-06 10:54:31 +0800

    ready

--- docker-start.sh
+++ docker-start.sh
@@ -1,1 +1,1 @@
-docker run -p 8080:80 -p 443:443 -v nginx.conf:/usr/local/nginx/conf/ -d nginx 
+docker run -p 8080:80 -p 443:443 -v nginx.conf:/etc/nginx/nginx.conf -v 3867642*:/usr/local/nginx/conf/ -d nginx 
